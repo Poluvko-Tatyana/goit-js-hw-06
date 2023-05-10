72b1685e5eb6dee243d3c3d3a148d60ec21a4292
Poluvko-Tatyana <poluvko.tatyana@gmail.com>
2023-05-10 19:25:42 +0300

    6 завдання

--- js/task-06.js
+++ js/task-06.js
@@ -4,18 +4,11 @@ inputEl.addEventListener("blur", onInput);
 
 function onInput (evt){
     if(evt.currentTarget.value.length === Number(dataLength)){
-        if(inputEl.classList.contains(invalid)){
-            inputEl.classList.remove(invalid)
-        }
+        inputEl.classList.remove(invalid);
         inputEl.classList.add(valid);
 
-    }else if(evt.currentTarget.value.length === 0){
-    if(inputEl.classList.contains(valid)){
-        inputEl.classList.remove(valid)
-    }
+    }else{
+    inputEl.classList.remove(valid);
     inputEl.classList.add(invalid);
 }
-    else {
-        inputEl.classList.add(invalid);
-    }
 }
---
+++ js/task-07.js
@@ -0,0 +1,8 @@
+const inputEl = document.querySelector('#font-size-control');
+const spanTextEl = document.querySelector("text");
+inputEl.addEventListener("input", onInput);
+
+function onInput (evt){
+    console (evt)
+    if(evt)
+}
