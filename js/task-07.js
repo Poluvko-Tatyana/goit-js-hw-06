980b400760f427486accf2b7c544d654faade7da
Poluvko-Tatyana <poluvko.tatyana@gmail.com>
2023-05-12 11:49:22 +0300

    07

--- js/task-07.js
+++ js/task-07.js
@@ -1,8 +1,10 @@
 const inputEl = document.querySelector('#font-size-control');
-const spanTextEl = document.querySelector("text");
+const spanTextEl = document.querySelector("#text");
 inputEl.addEventListener("input", onInput);
 
 function onInput (evt){
-    console (evt)
-    if(evt)
+    spanTextEl.style.fontSize = evt.currentTarget.valueAsNumber
+    console.dir(evt.currentTarget.valueAsNumber);
+    console.dir(spanTextEl.style.fontSize)
 }
+
